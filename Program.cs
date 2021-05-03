06b16cbed7b3c9d48eab5f92149dfe125d5c214e
Erick Cabrera <cornejoerick7@gmail.com>
2021-05-03 17:53:40 -0600

    Formulario de inicio

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace SistemaInventario
         /// </summary>
         [STAThread]
         static void Main()
-       {
+        {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmBienvenida());
